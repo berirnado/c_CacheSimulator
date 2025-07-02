af79990c7a4f8c3998b1efa312cd03681923d57f
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-07-02 17:13:50 -0300

    Arguments handling

--- cache_simulator.c
+++ cache_simulator.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // args: <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada
+// Exemplo 1  ["256", "4", "1", "R", "1", "bin_100.bin"] 
 
 int main(int argc, char **argv){
+
     if(argc != 7){
         printf("Invalid arguments\nExiting...");
         exit(1);
     }
+
+    // Number of Sets
+    const short int n_sets = atoi(argv[1]);
+
+    // Block Size
+    const short int b_size = atoi(argv[2]);
+
+    // Cache Associativity
+    const short int assoc = atoi(argv[3]);
+
+    // Replacement Policy
+    // R: random
+    // F: FIFO
+    // L: LRU
+    const char repPolicy = argv[4][0];
+
+    // Output Flag
+    // 0: with text
+    // 1: no text 
+    const short int outputFlag = atoi(argv[5]);
+
+    // Input File
+    // Addresses to benchmark
+    char *intputFile = argv[6];
+
     return 0;
 }
