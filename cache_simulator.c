3cea555f37ec38d57ec55bcca95aaec0a454ac5c
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-07-02 11:22:47 -0300

    Argc validation

---
+++ cache_simulator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// args: <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada
+
+int main(int argc, char **argv){
+    if(argc != 7){
+        printf("Invalid arguments\nExiting...");
+        exit(1);
+    }
+    return 0;
+}
