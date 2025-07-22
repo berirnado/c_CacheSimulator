77501e41ab066dcd9f689f37f53b734becb6c631
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-07-22 17:15:34 -0300

    Fixes index calc

--- cache_simulator.c
+++ cache_simulator.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
 
 // Function that converts big-endian to little-endian
 uint32_t big_to_little_endian(uint8_t bytes[4]) {
@@ -51,14 +53,59 @@ int main(int argc, char **argv){
         exit(1);
     }
 
-    char currentAddress[32];
+    //Data structure that will store the cache
+    bool cache_val[n_sets * assoc];
+    int cache_tag[n_sets * assoc];
 
-    int currentAdd;
+    int n_bits_offset = log2(b_size);
+    int n_bits_index = log2(n_sets);
+    int n_bits_tag = 32 - n_bits_offset - n_bits_index;
 
-    uint8_t buffer[4];  // stores the 4 bytes (sizeof(int))
+    // Counters for benchmark
+    int totalAccessCount = 0;
+    int totalMissCount = 0;
+    int totalHitCount = 0;
+    int compulsoryMissCount = 0;
+    int capacityMissCount = 0;
+    int conflictMissCount = 0;
+
+    uint8_t buffer[4];  // stores the 4 bytes (word size)
     while (fread(buffer, sizeof(uint8_t), 4, filePtr) == 4) {
-        uint32_t numero = big_to_little_endian(buffer);
+        uint32_t currentAdd = big_to_little_endian(buffer);
+        uint32_t tag = currentAdd >> (n_bits_offset + n_bits_index);
+        uint32_t index = (currentAdd >> n_bits_offset) & ((1 << n_bits_index) - 1);
+
+        printf("Add: 0x%08X | tag: 0x%08X | index: %u\n",
+       currentAdd,
+       tag,
+       index);
         //TODO: cache logic
+
+        totalAccessCount++;
+
+        //  Direct mapping (devemos mover isso pra uma função talvez)
+        //not valid (miss)
+        if(cache_val[index] == false){
+            totalMissCount++;
+            compulsoryMissCount++;
+
+            //Miss treatment (writes tag in index) (sets validity bit to 1)
+            cache_val[index] = true;
+            cache_tag[index] = tag;
+
+
+        }else{
+            if(cache_tag[index] == tag){
+                totalHitCount++; 
+            }else{
+                //conflito ou capacidade?
+                totalMissCount++;
+
+                //Miss treatment (writes tag in index) (sets validity bit to 1)
+                cache_val[index] = true;
+                cache_tag[index] = tag;
+            }
+        }
     }
 
     return 0;
